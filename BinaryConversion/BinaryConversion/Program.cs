1953fd574e8dc5ba06baf9c9cacd55ef94957dfe
wheat32 <npage@southhills.edu>
2022-11-21 10:15:20 -0500

    Initial commit

---
+++ BinaryConversion/BinaryConversion/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+using System;
+
+namespace BinaryConversion
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //TODO: Your code here
+        }
+    }
+}
